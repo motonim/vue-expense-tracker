79d8b4f2c1a88c50c5754efd5c64e61da70e0bc5
motonim <motonimjrp@gmail.com>
2024-01-02 16:56:03 -0500

    transaction component using shorter Composition API

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -10,24 +10,11 @@
     </ul>
 </template>
 
-<script> //options API
-export default {
-    setup() {
-        const transactions = [
-            { id: 1, text: 'Flower', amount: -19.99},
-            { id: 2, text: 'Salary', amount: 299.97},
-            { id: 3, text: 'Book', amount: -10},
-            { id: 4, text: 'Camera', amount: 150},
-        ];
-
-        const testFunction = () => {
-            console.log('test');
-        };
-
-        return {
-            transactions,
-            testFunction,
-        };
-    }
-}
+<script setup> //options API
+const transactions = [
+    { id: 1, text: 'Flower', amount: -19.99},
+    { id: 2, text: 'Salary', amount: 299.97},
+    { id: 3, text: 'Book', amount: -10},
+    { id: 4, text: 'Camera', amount: 150},
+];
 </script>
