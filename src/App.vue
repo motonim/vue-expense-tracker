f63c902802c0a899ea0c26e9e19018cf0a845c18
Jaeri <motonimjrp@gmail.com>
2024-01-03 17:04:56 -0500

    delete transaction

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <div class="container">
     <Balance :total="total"/>
     <IncomeExpenses :income="+income" :expenses="+expenses"/>
-    <TransactionList :transactions="transactions"/>
+    <TransactionList :transactions="transactions" @transactionDeleted="handleTransactionDeleted" />
     <AddTransaction @transactionSubmitted="handleTransactionSubmitted" />
   </div>
 </template>
@@ -70,5 +70,12 @@ const handleTransactionSubmitted = (transactionData) => {
 // Generate unique ID
 const generateUniqueId = () => {
   return Math.floor(Math.random() * 1000000);
+}
+
+// Delete transaction
+const handleTransactionDeleted = (id) => {
+  transactions.value = transactions.value.filter((transaction) => transaction.id !== id);
+
+  toast.success('Transaction deleted');
 }
 </script>
